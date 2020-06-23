27e8f29279894c62ac10cbfec8ddd8ae6d00d78f
Chris Marsh <cmhilfiker@gmail.com>
2020-06-23 10:47:38 +0000

    Form succesfully outputting entry to terminal

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print("yohohoh")
+        print(request.form)
     return render_template("contact.html",  page_title="Contact")
 
 
