7f03e34047ecc3113014f7315c72827c95a15ffb
Chris Marsh <cmhilfiker@gmail.com>
2020-06-23 09:46:05 +0000

    Add character data to JSON file and get working

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,9 +10,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company_data=data)
 
 
 @app.route("/contact")
